a865d47069be1818b46125c9fb9a44112c3b503c
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2025-07-30 16:55:33 -0300

    :lipstick: feat: Troca de fonte padrão para IBM Plex Sans

--- app/src/main/java/com/example/calculadoraimc/ui/theme/Color.kt
+++ app/src/main/java/com/example/calculadoraimc/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.example.calculadoraimc.ui.theme
 
+import android.graphics.ColorSpace.Rgb
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val WhiteTag = Color(0xFFFFFFFF)
+val BlueColor = Color(187, 231, 239)
+val GreenColor = Color(210, 235, 190)
+val GreyColor = Color(247, 247, 247)
+val BlackFont = Color(0xFF161616)
--- app/src/main/java/com/example/calculadoraimc/ui/theme/Type.kt
+++ app/src/main/java/com/example/calculadoraimc/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.example.calculadoraimc.R
@@ -14,10 +15,19 @@ val provider = GoogleFont.Provider(
     certificates = R.array.com_google_android_gms_fonts_certs
 )
 
+val fontName = GoogleFont("IBM Plex Sans")
+
+val fontFamilyIBM = FontFamily(
+    Font(
+        googleFont = fontName,
+        fontProvider = provider
+    )
+)
+
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamilyIBM,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,

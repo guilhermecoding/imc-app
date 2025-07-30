c35d6242c1073b1c36ec17040cd44b0c09c527e7
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2025-07-30 18:05:38 -0300

    :lipstick: feat: Aplicação de fontes localmente.

--- app/src/main/java/com/example/calculadoraimc/ui/theme/Type.kt
+++ app/src/main/java/com/example/calculadoraimc/ui/theme/Type.kt
@@ -2,51 +2,44 @@ package com.example.calculadoraimc.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.example.calculadoraimc.R
 
-val provider = GoogleFont.Provider(
-    providerAuthority = "com.google.android.gms.fonts",
-    providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs
-)
-
-val fontName = GoogleFont("IBM Plex Sans")
-
-val fontFamilyIBM = FontFamily(
-    Font(
-        googleFont = fontName,
-        fontProvider = provider
-    )
+val ibmPlexSansFamily = FontFamily(
+    Font(R.font.ibm_plex_sans_light, FontWeight.Light),
+    Font(R.font.ibm_plex_sans_extralight, FontWeight.ExtraLight),
+    Font(R.font.ibm_plex_sans_thin, FontWeight.Thin),
+    Font(R.font.ibm_plex_sans_medium, FontWeight.Medium),
+    Font(R.font.ibm_plex_sans_regular, FontWeight.Normal),
+    Font(R.font.ibm_plex_sans_semibold, FontWeight.SemiBold),
+    Font(R.font.ibm_plex_sans_bold, FontWeight.Bold)
 )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = fontFamilyIBM,
+        fontFamily = ibmPlexSansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = ibmPlexSansFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ibmPlexSansFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
